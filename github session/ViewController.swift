2f16521dcde42451ebaf8fc8b188076afc6f7f78
George Grant (s5114675) <s5114675@mac42938.student.bournemouth.ac.uk>
2019-04-03 11:32:19 +0100

    Initial commit

---
+++ github session/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github session
+//
+//  Created by George Grant (s5114675) on 03/04/2019.
+//  Copyright © 2019 George Grant (s5114675). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
